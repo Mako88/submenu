a1354b8dda8748975ad8b2b1bb5bc2faa0eef5cc
Mako88 <afuturepilotis@gmail.com>
2014-12-12 16:08:03 -0600

    Fixed Indentation

--- submenu.js
+++ submenu.js
@@ -10,45 +10,45 @@
 // scripts and/or other plugins which may not be closed properly.
 ;(function ( $, window, document, undefined ) {
 
-		// undefined is used here as the undefined global variable in ECMAScript 3 is
-		// mutable (ie. it can be changed by someone else). undefined isn't really being
-		// passed in so we can ensure the value of it is truly undefined. In ES5, undefined
-		// can no longer be modified.
+	// undefined is used here as the undefined global variable in ECMAScript 3 is
+	// mutable (ie. it can be changed by someone else). undefined isn't really being
+	// passed in so we can ensure the value of it is truly undefined. In ES5, undefined
+	// can no longer be modified.
 
-		// window and document are passed through as local variable rather than global
-		// as this (slightly) quickens the resolution process and can be more efficiently
-		// minified (especially when both are regularly referenced in your plugin).
+	// window and document are passed through as local variable rather than global
+	// as this (slightly) quickens the resolution process and can be more efficiently
+	// minified (especially when both are regularly referenced in your plugin).
 
-		// Create the defaults once
-		var pluginName = "submenu",
-				defaults = {
-				sub: "#submenu",
-                link: "#parentlink"
-		};
+	// Create the defaults once
+	var pluginName = "submenu",
+		defaults = {
+		sub: "#submenu",
+        	link: "#parentlink"
+	};
 
-		// The actual plugin constructor
-		function Plugin ( element, options ) {
-				this.element = element;
-				// jQuery has an extend method which merges the contents of two or
-				// more objects, storing the result in the first object. The first object
-				// is generally empty as we don't want to alter the default options for
-				// future instances of the plugin
-				this.settings = $.extend({}, defaults, options);
-                this.settings.sub = $(this.settings.sub);
-                this.settings.link = $(this.settings.link);
-				this._defaults = defaults;
-				this._name = pluginName;
-				this.init();
-		}
+	// The actual plugin constructor
+	function Plugin ( element, options ) {
+		this.element = element;
+		// jQuery has an extend method which merges the contents of two or
+		// more objects, storing the result in the first object. The first object
+		// is generally empty as we don't want to alter the default options for
+		// future instances of the plugin
+		this.settings = $.extend({}, defaults, options);
+        	this.settings.sub = $(this.settings.sub);
+        	this.settings.link = $(this.settings.link);
+		this._defaults = defaults;
+		this._name = pluginName;
+		this.init();
+	}
 
-		Plugin.prototype = {
-				init: function () {
-						// Place initialization logic here
-						// You already have access to the DOM element and
-						// the options via the instance, e.g. this.element
-						// and this.settings
-						// you can add more functions like the one below and
-						// call them like so: this.yourOtherFunction(this.element, this.settings).
+	Plugin.prototype = {
+		init: function () {
+			// Place initialization logic here
+			// You already have access to the DOM element and
+			// the options via the instance, e.g. this.element
+			// and this.settings
+			// you can add more functions like the one below and
+			// call them like so: this.yourOtherFunction(this.element, this.settings).
                         var sub = this.settings.sub, link = this.settings.link, parentmouse = false, submouse = false, subactive = false;
                         if('ontouchstart' in window || navigator.msMaxTouchPoints) {
                             $(this.element).on("touchstart", function () {
@@ -110,20 +110,20 @@
                                 submouse = false;
                             });
                         }
-				},
-		};
+		},
+	};
 
-		// A really lightweight plugin wrapper around the constructor,
-		// preventing against multiple instantiations
-		$.fn[ pluginName ] = function ( options ) {
-				this.each(function() {
-						if ( !$.data( this, "plugin_" + pluginName ) ) {
-								$.data( this, "plugin_" + pluginName, new Plugin( this, options ) );
-						}
-				});
+	// A really lightweight plugin wrapper around the constructor,
+	// preventing against multiple instantiations
+	$.fn[ pluginName ] = function ( options ) {
+		this.each(function() {
+			if ( !$.data( this, "plugin_" + pluginName ) ) {
+				$.data( this, "plugin_" + pluginName, new Plugin( this, options ) );
+			}
+		});
 
-				// chain jQuery functions
-				return this;
-		};
+		// chain jQuery functions
+		return this;
+	};
 
 })( jQuery, window, document );
